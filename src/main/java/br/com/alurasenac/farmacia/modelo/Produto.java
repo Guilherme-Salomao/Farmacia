2e577dde2cbb6a978c017e4971fff479f2755e11
Guilherme-Salomao <64329822+Guilherme-Salomao@users.noreply.github.com>
2024-02-26 22:10:49 -0300

    Classe Produto

---
+++ src/main/java/br/com/alurasenac/farmacia/modelo/Produto.java
@@ -0,0 +1,32 @@
+package br.com.alurasenac.farmacia.modelo;
+
+public class Produto {
+    private int id;
+    private double preco;
+    private String nome;
+    private String fabricante;
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+}
